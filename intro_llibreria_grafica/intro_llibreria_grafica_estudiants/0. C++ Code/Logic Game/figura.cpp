b2d65982830247211cc5ee22c92ac7d215185b59
NIU1709419 <149013299+NIU1709419@users.noreply.github.com>
2024-06-04 23:46:51 +0200

    figura.cpp acabada

--- intro_llibreria_grafica/intro_llibreria_grafica_estudiants/0. C++ Code/Logic Game/figura.cpp
+++ intro_llibreria_grafica/intro_llibreria_grafica_estudiants/0. C++ Code/Logic Game/figura.cpp
@@ -1,54 +1,318 @@
-#ifndef FIGURA_H
-#define FIGURA_H
-#include "InfoJoc.h"
+#include "Figura.h"
 
-IMAGE_NAME colorToPng(ColorFigura color);
-
-class Figura
+//Constructor predeterminat de Figura, inicialitza totes les dades de la classe
+Figura::Figura()
 {
-public:
-    Figura();
-    Figura(const TipusFigura& figura,const Posicio& pos,const int& numGir);
-    void inicialitza();
+	m_mida = 0;
+	m_fila = -1;
+	m_columna = -1;
+	m_gir = 0;
+	m_color = NO_COLOR;
+	m_tipusFigura = NO_FIGURA;
+	m_posicio.vertical = 0;
+	m_posicio.horitzontal = 0;
+	m_moviment = true;
+	for (int i = 0; i < MAX_ALCADA; i++)
+		for (int j = 0; j < MAX_AMPLADA; j++)
+			m_figura[i][j] = NO_COLOR;
 
-    ColorFigura getColor() const { return m_color; }
-    int getMida() const { return m_mida; }
-    ColorFigura getFigura(const int& x, const int& y) const { return m_figura[x][y]; }
-    int getCentreFila() const { return m_centreFila; }
-    int getCentreColumna() const { return m_centreColumna; }
-    Posicio getPosicio() const { return m_posicio; }
-    
-    void setPosicio(const Posicio& pos) { m_posicio = pos; }
-    void setTipus(const TipusFigura& tipus) { m_tipusFigura = tipus; }
-    void setFila(const int& fila) { m_fila = fila; }
-    void setColumna(const int& columna) { m_columna = columna; }
-    void setGir(const int& gir) { m_gir = gir; }
-    void setMoviment(const bool& moviment) { m_moviment = moviment; }
+}
 
-    void girar(const DireccioGir& gir);
-    void baixar(const int& dirY) { m_posicio.vertical += dirY; }
-    void desplacar(const int& dirX) { m_posicio.horitzontal += dirX; }
+//Constructor que inicialitza una figura concreta (O,I,T...)
+Figura::Figura(const TipusFigura& figura, const Posicio& pos, const int& numGir)
+{
+	m_mida = 0;
+	m_tipusFigura = figura;
+	m_posicio = pos;
 
-    void dibuixa(const bool& shadow) const;
-    void dibuixaFiguraSmall(const int& posCua) const;
+	//inicialitzem matriu de la figura
+	for (int i = 0; i < MAX_ALCADA; i++)
+		for (int j = 0; j < MAX_AMPLADA; j++)
+			m_figura[i][j] = NO_COLOR;
 
-private:
-      
-    TipusFigura m_tipusFigura; //forma de la figura
-    ColorFigura m_color;
-    Posicio m_posicio;
-    int m_mida;
-    int m_gir;
-    bool m_moviment;
-    ColorFigura m_figura[MAX_ALCADA][MAX_AMPLADA]; //matriu amb la forma de la figura i color
-    int m_fila;
-    int m_columna;
+	//configurem la mida i matriu de la figura segons el tipus
+	switch (m_tipusFigura)
+	{
+	case FIGURA_O:
+		m_mida = 2;
+		m_color = COLOR_GROC;
+		m_figura[0][0] = COLOR_GROC;
+		m_figura[0][1] = COLOR_GROC;
+		m_figura[1][0] = COLOR_GROC;
+		m_figura[1][1] = COLOR_GROC;
+		break;
+	case FIGURA_I:
+		m_mida = 4;
+		m_color = COLOR_BLAUCEL;
+		m_figura[1][0] = COLOR_BLAUCEL;
+		m_figura[1][1] = COLOR_BLAUCEL;
+		m_figura[1][2] = COLOR_BLAUCEL;
+		m_figura[1][3] = COLOR_BLAUCEL;
+		break;
+	case FIGURA_T:
+		m_mida = 3;
+		m_color = COLOR_MAGENTA;
+		m_figura[0][1] = COLOR_MAGENTA;
+		m_figura[1][0] = COLOR_MAGENTA;
+		m_figura[1][1] = COLOR_MAGENTA;
+		m_figura[1][2] = COLOR_MAGENTA;
+		break;
+	case FIGURA_L:
+		m_mida = 3;
+		m_color = COLOR_TARONJA;
+		m_figura[0][1] = COLOR_TARONJA;
+		m_figura[1][1] = COLOR_TARONJA;
+		m_figura[2][1] = COLOR_TARONJA;
+		m_figura[2][2] = COLOR_TARONJA;
+		break;
+	case FIGURA_J:
+		m_mida = 3;
+		m_color = COLOR_BLAUFOSC;
+		m_figura[0][1] = COLOR_BLAUFOSC;
+		m_figura[1][1] = COLOR_BLAUFOSC;
+		m_figura[2][0] = COLOR_BLAUFOSC;
+		m_figura[2][1] = COLOR_BLAUFOSC;
+		break;
+	case FIGURA_Z:
+		m_mida = 3;
+		m_color = COLOR_VERMELL;
+		m_figura[0][0] = COLOR_VERMELL;
+		m_figura[0][1] = COLOR_VERMELL;
+		m_figura[1][1] = COLOR_VERMELL;
+		m_figura[1][2] = COLOR_VERMELL;
+		break;
+	case FIGURA_S:
+		m_mida = 3;
+		m_color = COLOR_VERD;
+		m_figura[0][1] = COLOR_VERD;
+		m_figura[0][2] = COLOR_VERD;
+		m_figura[1][0] = COLOR_VERD;
+		m_figura[1][1] = COLOR_VERD;
+		break;
+	}
 
-    const int m_centreFila = 1;
-    const int m_centreColumna = 1;
+	//gira la figura en sentit horari el nombre de vegades que indica la variable
+	for (int i = 0; i < numGir; i++)
+		girar(GIR_HORARI);
+}
 
-};
+//Inicialitza una figura com amb el constructor, però permet reinicialitzar una figura existent amb un nou tipus
+void Figura::inicialitza()
+{
+	
+	//inicialitzem matriu de la figura
+	for (int i = 0; i < MAX_ALCADA; i++)
+		for (int j = 0; j < MAX_AMPLADA; j++)
+			m_figura[i][j] = NO_COLOR;
 
-ifstream& operator>>(ifstream& input, Figura& figura);
+	//configurem la mida i matriu de la figura segons el tipus
+	switch (m_tipusFigura)
+	{
+	case FIGURA_O:
+		m_mida = 2;
+		m_color = COLOR_GROC;
+		m_figura[0][0] = COLOR_GROC;
+		m_figura[0][1] = COLOR_GROC;
+		m_figura[1][0] = COLOR_GROC;
+		m_figura[1][1] = COLOR_GROC;
+		break;
+	case FIGURA_I:
+		m_mida = 4;
+		m_color = COLOR_BLAUCEL;
+		m_figura[1][0] = COLOR_BLAUCEL;
+		m_figura[1][1] = COLOR_BLAUCEL;
+		m_figura[1][2] = COLOR_BLAUCEL;
+		m_figura[1][3] = COLOR_BLAUCEL;
+		break;
+	case FIGURA_T:
+		m_mida = 3;
+		m_color = COLOR_MAGENTA;
+		m_figura[0][1] = COLOR_MAGENTA;
+		m_figura[1][0] = COLOR_MAGENTA;
+		m_figura[1][1] = COLOR_MAGENTA;
+		m_figura[1][2] = COLOR_MAGENTA;
+		break;
+	case FIGURA_L:
+		m_mida = 3;
+		m_color = COLOR_TARONJA;
+		m_figura[0][1] = COLOR_TARONJA;
+		m_figura[1][1] = COLOR_TARONJA;
+		m_figura[2][1] = COLOR_TARONJA;
+		m_figura[2][2] = COLOR_TARONJA;
+		break;
+	case FIGURA_J:
+		m_mida = 3;
+		m_color = COLOR_BLAUFOSC;
+		m_figura[0][1] = COLOR_BLAUFOSC;
+		m_figura[1][1] = COLOR_BLAUFOSC;
+		m_figura[2][0] = COLOR_BLAUFOSC;
+		m_figura[2][1] = COLOR_BLAUFOSC;
+		break;
+	case FIGURA_Z:
+		m_mida = 3;
+		m_color = COLOR_VERMELL;
+		m_figura[0][0] = COLOR_VERMELL;
+		m_figura[0][1] = COLOR_VERMELL;
+		m_figura[1][1] = COLOR_VERMELL;
+		m_figura[1][2] = COLOR_VERMELL;
+		break;
+	case FIGURA_S:
+		m_mida = 3;
+		m_color = COLOR_VERD;
+		m_figura[0][1] = COLOR_VERD;
+		m_figura[0][2] = COLOR_VERD;
+		m_figura[1][0] = COLOR_VERD;
+		m_figura[1][1] = COLOR_VERD;
+		break;
+	}
 
-#endif
+}
+
+IMAGE_NAME colorToPng(ColorFigura color)
+{
+	switch (color)
+	{
+	case COLOR_GROC:    return GRAFIC_QUADRAT_GROC;
+		break;
+	case COLOR_BLAUCEL: return GRAFIC_QUADRAT_BLAUCEL;
+		break;
+	case COLOR_BLAUFOSC:return GRAFIC_QUADRAT_BLAUFOSC;
+		break;
+	case COLOR_MAGENTA: return GRAFIC_QUADRAT_MAGENTA;
+		break;
+	case COLOR_TARONJA: return GRAFIC_QUADRAT_TARONJA;
+		break;
+	case COLOR_VERD:    return GRAFIC_QUADRAT_VERD;
+		break;
+	case COLOR_VERMELL: return GRAFIC_QUADRAT_VERMELL;
+		break;
+	default:    return GRAFIC_FONS;
+		break;
+	}
+}
+
+IMAGE_NAME colorToPng_shadow(ColorFigura color)
+{
+	switch (color)
+	{
+	case COLOR_GROC:    return GRAFIC_QUADRAT_GROC_SHADOW;
+		break;
+	case COLOR_BLAUCEL: return GRAFIC_QUADRAT_BLAUCEL_SHADOW;
+		break;
+	case COLOR_BLAUFOSC:return GRAFIC_QUADRAT_BLAUFOSC_SHADOW;
+		break;
+	case COLOR_MAGENTA: return GRAFIC_QUADRAT_MAGENTA_SHADOW;
+		break;
+	case COLOR_TARONJA: return GRAFIC_QUADRAT_TARONJA_SHADOW;
+		break;
+	case COLOR_VERD:    return GRAFIC_QUADRAT_VERD_SHADOW;
+		break;
+	case COLOR_VERMELL: return GRAFIC_QUADRAT_VERMELL_SHADOW;
+		break;
+	default:	return GRAFIC_FONS;
+		break;
+	}
+}
+
+IMAGE_NAME colorToPng_small(ColorFigura color)
+{
+	switch (color)
+	{
+	case COLOR_GROC:    return GRAFIC_QUADRAT_GROC_SMALL;
+		break;
+	case COLOR_BLAUCEL: return GRAFIC_QUADRAT_BLAUCEL_SMALL;
+		break;
+	case COLOR_BLAUFOSC:return GRAFIC_QUADRAT_BLAUFOSC_SMALL;
+		break;
+	case COLOR_MAGENTA: return GRAFIC_QUADRAT_MAGENTA_SMALL;
+		break;
+	case COLOR_TARONJA: return GRAFIC_QUADRAT_TARONJA_SMALL;
+		break;
+	case COLOR_VERD:    return GRAFIC_QUADRAT_VERD_SMALL;
+		break;
+	case COLOR_VERMELL: return GRAFIC_QUADRAT_VERMELL_SMALL;
+		break;
+	default:	return GRAFIC_FONS;
+		break;
+	}
+}
+
+void Figura::dibuixa(const bool& shadow) const
+{
+	IMAGE_NAME color;
+
+	if (shadow)
+		color = colorToPng_shadow(m_color);
+	else
+		color = colorToPng(m_color);
+
+	for (int i = 0; i < m_mida; i++)
+	{
+		for (int j = 0; j < m_mida; j++)
+		{
+			if (m_figura[i][j] != NO_COLOR)
+				GraphicManager::getInstance()->drawSprite(color, 
+					POS_X_TAULER + ((m_posicio.horitzontal + 1 + j) * MIDA_QUADRAT),
+					POS_Y_TAULER + ((m_posicio.vertical + i) * MIDA_QUADRAT), false);
+		}
+	}
+}
+
+void Figura::dibuixaFiguraSmall(const int& posCua) const
+{
+	IMAGE_NAME color = colorToPng_small(m_color);
+
+	for (int i = 0; i < m_mida; i++)
+	{
+		for (int j = 0; j < m_mida; j++)
+		{
+			if (m_figura[i][j] != NO_COLOR)
+				GraphicManager::getInstance()->drawSprite(color,
+					POS_X_TAULER + 380 + (j * MIDA_QUADRAT_SMALL),
+					POS_Y_TAULER + (posCua * 70) + (i * MIDA_QUADRAT_SMALL), false);
+		}
+	}
+}
+
+//gira la figura en la direccióGir (horaria o antihoraria)
+void Figura::girar(const DireccioGir& gir)
+{
+	ColorFigura transposada[MAX_ALCADA][MAX_AMPLADA];
+	ColorFigura invertir[MAX_ALCADA][MAX_AMPLADA];
+
+	// Transposa matriu
+	for (int i = 0; i < m_mida; ++i)
+		for (int j = 0; j < m_mida; ++j)
+			transposada[j][i] = m_figura[i][j];
+
+	for (int i = 0; i < m_mida; ++i)
+		for (int j = 0; j < m_mida; ++j)
+			m_figura[i][j] = transposada[i][j];
+
+	//Inverteix matriu segons la direcció de gir
+	if (gir == GIR_HORARI)
+	{
+		for (int i = 0; i < m_mida; ++i)
+			for (int j = 0; j < m_mida; ++j)
+				invertir[i][j] = m_figura[i][m_mida - 1 - j];
+	}
+	else
+		for (int i = 0; i < m_mida; ++i)
+			for (int j = 0; j < m_mida; ++j)
+				invertir[i][j] = m_figura[m_mida - 1 - i][j];
+
+	for (int i = 0; i < m_mida; ++i)
+		for (int j = 0; j < m_mida; ++j)
+			m_figura[i][j] = invertir[i][j];
+}
+
+ifstream& operator>>(ifstream& input, Figura& figura)
+{
+	int tipus, fila, columna, gir;
+	input >> tipus >> fila >> columna >> gir;
+	figura.setTipus((TipusFigura)tipus);
+	figura.setFila(fila);
+	figura.setColumna(columna);
+	figura.setGir(gir);
+	return input;
+}
